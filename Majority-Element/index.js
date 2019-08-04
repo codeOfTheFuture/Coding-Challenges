1c3bfcde2a2d8c0c4d8c09606b41679b26888cc9
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-03 22:59:59 -0400

    fixed majority-element correct solution

--- Majority-Element/index.js
+++ Majority-Element/index.js
@@ -8,23 +8,28 @@
 // majorityElement([2, 2, 1, 1, 1, 2, 2]); <<<<<<< should return 2
 // majorityElement([8, 4, 8, 8, 8, 5, 5, 4]) <<<<<< should return 8
 
-function majorityElement(arr) {
-  const testArr = [];
+function majorityElement(nums) {
+  let majElMap = {};
+  let maxEl = nums[0],
+    maxCount = 1;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (testArr.includes(arr[i])) {
-      let spliceNum = arr.splice(arr.indexOf(arr[i]), 1);
-      testArr.push(spliceNum);
+  for (let i = 0; i < nums.length; i++) {
+    let el = nums[i];
+    if (!majElMap[el]) {
+      majElMap[el] = 1;
+    } else {
+      majElMap[el]++;
+    }
+    if (majElMap[el] > maxCount) {
+      maxEl = el;
+      maxCount = majElMap[el];
     }
   }
 
-  if (testArr.length > arr.length) {
-    return testArr[0];
-  } else {
-    return arr[0];
-  }
+  return maxEl;
 }
 
 console.log(majorityElement([3, 2, 3]));
+console.log(majorityElement([6, 5, 5]));
+console.log(majorityElement([3, 3, 4]));
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
-console.log(majorityElement([8, 4, 8, 8, 8, 5, 5, 4]));
